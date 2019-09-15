5289cec9df42920cf1fb4eb065f1ed8da2eeb1b6
magnorion <lucas.fulldev@gmail.com>
2019-09-15 20:29:12 -0300

    testando primeira classe de resources

--- src/main/java/com/magnorion/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/magnorion/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,26 @@
 package com.magnorion.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.magnorion.cursomc.domain.CategoriaDomain;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest esta funcionando";
+	public List<CategoriaDomain> listar() {
+		CategoriaDomain cat1 = new CategoriaDomain(1, "Informatica");
+		CategoriaDomain cat2 = new CategoriaDomain(2, "Papelaria");
+		
+		List<CategoriaDomain> lista = new ArrayList<CategoriaDomain>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
